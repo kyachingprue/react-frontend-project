366bce4c0a9967cb3e7420d7d69f89871e52fe79
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-06-20 23:19:58 +0600

    final commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,20 @@
 
 import Navbar from '../Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigation } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 
 const Home = () => {
+  const navigation = useNavigation();
+  const location = useLocation();
+  console.log(location);;
   return (
     <div>
       <Navbar />
-      <Outlet />
+      {
+        navigation.state === 'loading' ? <span className="loading loading-bars mx-36 mt-28 loading-xl"></span>
+          :
+          <Outlet />
+      }
       <Footer />
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <li><NavLink to="/explore">Explore</NavLink></li>
     <li><NavLink to="/about">About</NavLink></li>
     <li><NavLink to="/contact">Contact</NavLink></li>
-    <li><NavLink to="/posts">Post</NavLink></li>
+    <li><NavLink to="/posts">Posts</NavLink></li>
     <li><NavLink to="/users">Users</NavLink></li>
   </div>
   return (
